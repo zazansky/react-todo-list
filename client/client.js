0d49f831dec1d880ea31c745d3f704c32c0c03df
Slava Zazansky <zazansky@hotmail.com>
2017-04-06 12:12:44 -0400

    Part 3 manual additions

--- client/client.js
+++ client/client.js
@@ -1,1 +1,17 @@
+import React from 'react'
+//var React = require('react');
+
+import { render } from 'react-dom'
+//var render = require('react-dom').render;
+
+import App from'../components/App'
+
+render(
+    // define the encompassing component
+    // DOM element we want to mount it to
+    <App/>,
+    document.getElementById('app')
+)
+
+
 
